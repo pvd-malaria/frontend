f8e7c631e01b138256cc3771c2ea1337a107db16
Willianson <contato@willianson.com.br>
2022-03-30 08:43:47 -0300

    chore(UiGuide): adiciona TAG referencia.

--- src/pages/UiGuide/index.tsx
+++ src/pages/UiGuide/index.tsx
@@ -6,15 +6,15 @@ import Layout from '../../components/Layout';
 function UiGuide() {
   return (
     <Layout>
-      <h1>.title.bold</h1>
-      <h2>.title.regular</h2>
-      <h3>.title.light</h3>
-      <h4>.subtitle.regular</h4>
-      <h5>.subtitle.light</h5>
-      <p><strong>.paragraph.bold</strong></p>
-      <p>.paragraph.regular</p>
-      <small>.small.light</small>
-      <small>.small.light</small>
+      <h1>h1: .title.bold</h1>
+      <h2>h2: .title.regular</h2>
+      <h3>h3: .title.light</h3>
+      <h4>h4: .subtitle.regular</h4>
+      <h5>h5: .subtitle.light</h5>
+      <p><strong>p: .paragraph.bold</strong></p>
+      <p>p: .paragraph.regular</p>
+      <small>small: .small.light</small>
+      <small>small: .small.light</small>
     </Layout>
   );
 }
