9430e41960626eda91aba919bed15b9948b82210
Willianson <contato@willianson.com.br>
2022-04-02 22:36:12 -0300

    style: footer.
    - Adiciona logos SVG.
    - Adiciona estilo aos labels.
    - Adiciona filtro grayscale para imagens, e hover colore.
    - Altera de linha para column quando mobile.
    - Regra especifica para logo sus, por precisar ser mais largo que outros.
    
    closes #11

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,31 +1,59 @@
 import { Link } from 'react-router-dom';
 
+import LogoUnicamp from './../../../images/logo-unicamp.svg';
+import LogoInstitutoFederal from './../../../images/logo-instituto-federal-sao-paulo-campus-campinas.svg';
+import LogoUnesp from './../../../images/logo-unesp.svg';
+import LogoLaddem from './../../../images/logo-laddem.png';
+import LogoNepo from './../../../images/logo-nepo.svg';
+import LogoFuncamp from './../../../images/logo-funcamp.svg';
+import LogoFiotec from './../../../images/logo-fiotec.svg';
+import LogoBilleMelindaGates from './../../../images/logo-bill-melinda-gates-foundation.svg';
+import LogoCNPq from './../../../images/logo-cnpq.svg';
+// import LogoDecit from './../../../images/logo-decit.svg';
+import LogoSUS from './../../../images/logo-sus-ministerio-da-saude.svg';
+
 import styles from './styles.module.scss';
 
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <section>
-        <h3 className="title">Execução</h3>
-        <div className="item"><Link to="/"><img src="" alt="Unicamp" /></Link></div>
-        <div className="item"><Link to="/"><img src="" alt="Instituto Federal" /></Link></div>
-        <div className="item"><Link to="/"><img src="" alt="Unesp" /></Link></div>
-        <div className="item"><Link to="/"><img src="" alt="Laddem" /></Link></div>
-        <div className="item"><Link to="/"><img src="" alt="Nepo" /></Link></div>
-      </section>
-      <section>
-        <h3 className="title">Apoio</h3>
-        <div className="item"><Link to="/"><img src="" alt="Funcamp" /></Link></div>
-        <div className="item"><Link to="/"><img src="" alt="Fiotec" /></Link></div>
-      </section>
-      <section>
-        <h3 className="title">Financiamento</h3>
-        <div className="item"><Link to="/"><img src="" alt="Bill & Melinda Gates" /></Link></div>
-        <div className="item"><Link to="/"><img src="" alt="CNPq" /></Link></div>
-        <div className="item"><Link to="/"><img src="" alt="Decit" /></Link></div>
-        <div className="item"><Link to="/"><img src="" alt="SUS" /></Link></div>
-      </section>
+      <div className="container">
+        <section>
+          <div className={styles.info}>
+            <span className="title">Execução</span>
+          </div>
+          <div className={styles.items}>
+            <Link to="/"><img src={LogoUnicamp} alt="Unicamp" /></Link>
+            <Link to="/"><img src={LogoInstitutoFederal} alt="Instituto Federal" /></Link>
+            <Link to="/"><img src={LogoUnesp} alt="Unesp" /></Link>
+            <Link to="/"><img src={LogoLaddem} alt="Laddem" /></Link>
+            <Link to="/"><img src={LogoNepo} alt="Nepo" /></Link>
+          </div>
+        </section>
+
+        <section>
+          <div className={styles.info}>
+            <span className="title">Apoio</span>
+          </div>
+          <div className={styles.items}>
+            <Link to="/"><img src={LogoFuncamp} alt="Funcamp" /></Link>
+            <Link to="/"><img src={LogoFiotec} alt="Fiotec" /></Link>
+          </div>
+        </section>
+        
+        <section>
+          <div className={styles.info}>
+            <span className="title">Financiamento</span>
+          </div>
+          <div className={styles.items}>
+            <Link to="/"><img src={LogoBilleMelindaGates} alt="Bill & Melinda Gates" /></Link>
+            <Link to="/"><img src={LogoCNPq} alt="CNPq" /></Link>
+            <Link to="/"><img src="" alt="Decit" /></Link>
+            <Link to="/"><img src={LogoSUS} alt="SUS" className={styles.logoSus} /></Link>
+          </div>
+        </section>
+      </div>
     </footer>
   );
 }
