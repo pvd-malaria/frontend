c159f3243c09f54019aaf2b4f289ca2a5ae003ee
Willianson <contato@willianson.com.br>
2022-04-05 19:30:57 -0300

    feat: estrutura de textos para página inicial.
    - Estrutura de tags
    - titulo e textos
    - CSS base.
    
    #12

--- src/pages/Inicial/index.tsx
+++ src/pages/Inicial/index.tsx
@@ -20,24 +20,59 @@ function Inicial() {
         </div>
       </section>
 
-      <section className="container destaque taxas">
+      <section className={classnames("container", styles.highlight)}>
         <h4>Taxas</h4>
         <h3>Lorem ipsum dolor</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad minim veniam, quis ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
         <Link to={'/'}>Veja mais</Link>
       </section>
 
-      <section className="container destaque taxas">
+      <section className={classnames("container", styles.highlight)}>
         <h4>Importados</h4>
         <h3>Lorem ipsum dolor</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad minim veniam, quis ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
         <Link to={'/'}>Veja mais</Link>
       </section>
 
-      <section className="container visualizacoes">
+      <section className={classnames("container", styles.highlight, styles.visualizations)}>
         <h4>Visualizações</h4>
         <p>Ut enim ad minim veniam, quis nostrud exercitationullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <ul className="items">
+          <li>
+            <img src="" alt="Visualização" />
+            <small>Ut enim ad minim veniam, quis nostrud exercitationullamco laboris commodo consequat.</small>
+          </li>
+          <li>
+            <img src="" alt="Visualização" />
+            <small>Ut enim ad minim veniam, quis nostrud exercitationullamco laboris commodo consequat.</small>
+          </li>
+          <li>
+            <img src="" alt="Visualização" />
+            <small>Ut enim ad minim veniam, quis nostrud exercitationullamco laboris commodo consequat.</small>
+          </li>
+        </ul>
+        <Link to="/">Mais visualizações</Link>
       </section>
+
+      <section className={classnames("container", styles.highlight, styles.quality)}>
+        <h4>Qualidade da Informação</h4>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velitesse cillum dolore eu fugiat nulla pariatur. Excepteur sintoccaecat cupidatat non proident, sunt in qui officiadeserunt mollit est laborum.</p>
+      </section>
+
+      <section className={classnames("container", styles.highlight, styles.publications)}>
+        <h4>Publicações</h4>
+        <article>
+          <h1>Lorem ipsum dolor sit amet, consectetur adipiscing</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad minim veniam, quis ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velitesse cillum dolore eu fugiat nulla pariatur.</p>
+        </article>
+        <article>
+          <h1>Lorem ipsum dolor sit amet, consectetur adipiscing</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad minim veniam, quis ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velitesse cillum dolore eu fugiat nulla pariatur.</p>
+        </article>
+        <Link to="/">Mais publicações</Link>
+      </section>
+
+
     </Layout>
   );
 }
