35af518499239f43ed6ec1dbad0da3c8b43e95fa
Willianson <contato@willianson.com.br>
2022-04-07 21:31:53 -0300

    refact(footer): melhorias.

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -9,21 +9,20 @@ import LogoFuncamp from './../../../images/logo-funcamp.svg';
 import LogoFiotec from './../../../images/logo-fiotec.svg';
 import LogoBilleMelindaGates from './../../../images/logo-bill-melinda-gates-foundation.svg';
 import LogoCNPq from './../../../images/logo-cnpq.svg';
-// import LogoDecit from './../../../images/logo-decit.svg';
 import LogoSUS from './../../../images/logo-sus-ministerio-da-saude.svg';
 
-import styles from './styles.module.scss';
+import './styles.css';
 
 
 function Footer() {
   return (
-    <footer className={styles.footer}>
+    <footer id="footer">
       <div className="container">
         <section>
-          <div className={styles.info}>
+          <div className="info">
             <span className="title">Execução</span>
           </div>
-          <div className={styles.items}>
+          <div className="items">
             <Link to="/"><img src={LogoUnicamp} alt="Unicamp" /></Link>
             <Link to="/"><img src={LogoInstitutoFederal} alt="Instituto Federal" /></Link>
             <Link to="/"><img src={LogoUnesp} alt="Unesp" /></Link>
@@ -33,24 +32,23 @@ function Footer() {
         </section>
 
         <section>
-          <div className={styles.info}>
+          <div className="info">
             <span className="title">Apoio</span>
           </div>
-          <div className={styles.items}>
+          <div className="items">
             <Link to="/"><img src={LogoFuncamp} alt="Funcamp" /></Link>
             <Link to="/"><img src={LogoFiotec} alt="Fiotec" /></Link>
           </div>
         </section>
         
         <section>
-          <div className={styles.info}>
+          <div className="info">
             <span className="title">Financiamento</span>
           </div>
-          <div className={styles.items}>
+          <div className="items">
             <Link to="/"><img src={LogoBilleMelindaGates} alt="Bill & Melinda Gates" /></Link>
             <Link to="/"><img src={LogoCNPq} alt="CNPq" /></Link>
-            <Link to="/"><img src="" alt="Decit" /></Link>
-            <Link to="/"><img src={LogoSUS} alt="SUS" className={styles.logoSus} /></Link>
+            <Link to="/"><img src={LogoSUS} alt="SUS" className="logoSus" /></Link>
           </div>
         </section>
       </div>
