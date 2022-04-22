40c07b22ff863b84cffffdbe13e1d4a7a83d2e48
Willianson <contato@willianson.com.br>
2022-04-21 21:59:24 -0300

    feat(subMenu): estrutura de sub-menu mobile & desktop.
    - Altera ordem de itens do menu.
    - Adiciona estrutura de submenus.
    - Adiciona icone ao lado de links com submenu.
    - Atualiza CSS com regras para submenu e parent com submenu.
    
    closes #18

--- src/components/Layout/Header/Navigation/index.tsx
+++ src/components/Layout/Header/Navigation/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 import { AppContext } from '../../../../contexts/AppContext';
 import CustomLink from './CustomLink';
@@ -26,24 +27,70 @@ function Navigation() {
             Sobre
           </CustomLink>
         </li>
-        <li>
+        <li className="hasSubmenu">
           <CustomLink activeClassName="active" to="/dashboard">
-            Dashboard
+            Dashboard <KeyboardArrowDownIcon/>
           </CustomLink>
-        </li>
-        <li>
-          <CustomLink activeClassName="active" to="/modelos">
-            Modelos
-          </CustomLink>
-        </li>
-        <li>
-          <CustomLink activeClassName="active" to="/producoes">
-            Produções
-          </CustomLink>
+          <ul className="submenu">
+            <li>
+              <CustomLink activeClassName="active" to="/dashboard/importados">
+                Importados
+              </CustomLink>
+              </li>
+            <li>
+              <CustomLink activeClassName="active" to="/dashboard/taxas">
+                Taxas
+              </CustomLink>
+            </li>
+          </ul>
         </li>
         <li>
           <CustomLink activeClassName="active" to="/visualizacoes">
             Visualizações
+          </CustomLink>
+        </li>
+        <li className="hasSubmenu">
+          <CustomLink activeClassName="active" to="/modelos">
+            Modelos <KeyboardArrowDownIcon/>
+          </CustomLink>
+          <ul className="submenu">
+              <li>
+                <CustomLink activeClassName="active" to="/modelos/classificacao">
+                  Classificação
+                </CustomLink>
+                </li>
+              <li>
+                <CustomLink activeClassName="active" to="/modelos/preditivos">
+                  Preditivos
+                </CustomLink>
+              </li>
+            </ul>
+        </li>
+        <li className="hasSubmenu">
+          <CustomLink activeClassName="active" to="/producoes">
+            Produções <KeyboardArrowDownIcon/>
+          </CustomLink>
+          <ul className="submenu">
+              <li>
+                <CustomLink activeClassName="active" to="/producoes/artigos">
+                  Artigos
+                </CustomLink>
+                </li>
+              <li>
+                <CustomLink activeClassName="active" to="/producoes/boletins">
+                  Boletins
+                </CustomLink>
+              </li>
+              <li>
+                <CustomLink activeClassName="active" to="/producoes/cursos">
+                  Cursos
+                </CustomLink>
+              </li>
+            </ul>
+        </li>
+        <li>
+          <CustomLink activeClassName="active" to="/contato">
+            Contato
           </CustomLink>
         </li>
       </ul>
