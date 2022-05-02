64934e4c92e2382e3b4a2957f9753d783dbc432f
Willianson <contato@willianson.com.br>
2022-05-02 08:01:11 -0300

    feat: importação de textos de arquivo JSON centralizado.
    - Adiciona arquivo contents com textos.
    - Adiciona funções no AppContext para obter os textos do arquivo.
    
    #19

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -4,8 +4,16 @@ import {
   useCallback,
 } from 'react';
 
+import { Interweave } from 'interweave';
+
+import contentsJson from '../contents/contents.json';
+
+
+type ContentsProperty = 'id' | 'url' | 'title' | 'short' | 'description';
 
 interface IAppContext {
+  getHtmlFromContents: (id: string, property: ContentsProperty, tag?: string) => JSX.Element;
+  getItemFromContents: (id: string, property: ContentsProperty) => string;
   navigationOpen: () => void;
   navigationClose: () => void;
 }
@@ -20,6 +28,16 @@ export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export function AppContextProvider(props: IAppContextProviderProps) {
 
+  const getHtmlFromContents = useCallback((id: string, property: ContentsProperty, tag?: string): JSX.Element => {
+    const filteredItem = contentsJson.filter(item => item.id === id)[0];   
+    return <Interweave noWrap content={filteredItem[property]} />;
+  }, []);
+
+  const getItemFromContents = useCallback((id: string, property: ContentsProperty): string => {
+    const filteredItem = contentsJson.filter(item => item.id === id)[0];   
+    return filteredItem[property];
+  }, []);
+
   const navigationOpen = useCallback(() => {
     document.querySelector('body')?.classList.add('navigation-open');
     document.querySelector('body')?.classList.add('no-scroll');
@@ -33,6 +51,8 @@ export function AppContextProvider(props: IAppContextProviderProps) {
 
   return (
     <AppContext.Provider value={{
+      getHtmlFromContents,
+      getItemFromContents,
       navigationOpen,
       navigationClose,
     }}>
