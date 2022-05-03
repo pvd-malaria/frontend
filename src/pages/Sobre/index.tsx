0ea7701e7c403f41fb91daee52bff270ebb30405
Willianson <contato@willianson.com.br>
2022-05-03 07:46:58 -0300

    feat(sobre): exibe textos baseado em JSON.
    - Substitui textos estáticos por funcao que busca texto em JSON contents.
    - Remove estilo de colunas da parte sobre o projeto.
    - Ajusta IDs de elementos no arquivo JSON.
    
    #19

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -1,55 +1,33 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import { AppContext } from '../../contexts/AppContext';
 import Layout from '../../components/Layout';
 
 import './styles.css';
 
 
 function Sobre() {
+
+  const { getHtmlFromContents, getItemFromContents } = useContext(AppContext);
+
   return (
     <Layout id="pageSobre">
 
       <section className="resumo">
         <div className="container">
-          <h1>Sobre o projeto Malária</h1>
+          <h1>{getItemFromContents('pagina_sobre', 'title')}</h1>
           <div className="info">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do 
-              eiusmod tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad
-              ad minim veniam, quis ea commodo consequat. Duis aute irure dolor 
-              in reprehenderit in voluptate.
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, 
-              odio repellat itaque molestiae laboriosam corrupti, recusandae voluptates ad
-              commodi quo porro soluta ullam veritatis quisquam amet? Pariatur 
-              consequuntur fuga velit beatae.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi 
-              mollitia odit molestias commodi iste fugit eius pariatur hic quos ad
-              sequi ipsam beatae nemo vero ad, suscipit earum quisquam expedita 
-              quod? sed do eiusmod tempor incididunt ut labore et dolore magnaaliqua.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias 
-              possimus ipsum rem adipisci unde dolore odio! Enim eaque sapiente ad
-              explicabo blanditiis nisi sequi ullam, sit odit quam aliquid temporibus 
-              deleniti?
-            </p>
+            {getHtmlFromContents('pagina_sobre', 'description')}
           </div>
         </div>
       </section>
 
       <section className="equipe">
         <div className="container">
-          <h2>Sobre a equipe</h2>
+          <h2>{getItemFromContents('pagina_sobre_equipe', 'title')}</h2>
           <div className="info">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod 
-              tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad minim 
-              veniam, quis nostrud exercitationullamco.
-            </p>
+            <p>{getItemFromContents('pagina_sobre_equipe', 'description')}</p>
           </div>
         </div>
 
@@ -149,16 +127,13 @@ function Sobre() {
 
       <section className="convite">
         <div className="container">
-          <h1>Conheça o projeto</h1>
+          <h1>{getItemFromContents('pagina_sobre_projeto', 'title')}</h1>
           <div className="info">
+            {getHtmlFromContents('pagina_sobre_projeto', 'description')}
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do 
-              eiusmod tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad
-              ad minim veniam, quis ea commodo consequat. Duis aute irure dolor 
-              in reprehenderit in voluptate.
-            </p>
-            <p>
-              <Link to="#">Lorem ipsum dolor</Link>
+              <Link to={getItemFromContents('pagina_sobre_projeto', 'url')}>
+                Lorem ipsum dolor
+              </Link>
             </p>
           </div>
         </div>
