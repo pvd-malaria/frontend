97521d0496695fd8f52ebba1f70827f09cf81ed3
Willianson <contato@willianson.com.br>
2022-05-11 07:35:07 -0300

    refact(textos): simplifica importação de textos em paginas.
    - Para simplificar manutenção e entendimento da arquitetura.

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -4,16 +4,8 @@ import {
   useCallback,
 } from 'react';
 
-import { Interweave } from 'interweave';
-
-import contentsJson from '../contents/contents.json';
-
-
-type ContentsProperty = 'id' | 'url' | 'title' | 'short' | 'description';
 
 interface IAppContext {
-  getHtmlFromContents: (id: string, property: ContentsProperty, tag?: string) => JSX.Element;
-  getItemFromContents: (id: string, property: ContentsProperty) => string;
   navigationOpen: () => void;
   navigationClose: () => void;
 }
@@ -27,33 +19,6 @@ export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 
 export function AppContextProvider(props: IAppContextProviderProps) {
-
-  const getHtmlFromContents = useCallback((id: string, property: ContentsProperty, tag?: string): JSX.Element => {
-    let output: JSX.Element;
-    const filteredItem = contentsJson.filter(item => item.id === id);   
-
-    if (filteredItem.length === 0) {
-      output = <>Item "{id}.{property}" nao encontrado em contents.json</>;
-    } else {
-      output = <Interweave noWrap content={filteredItem[0][property]} />;
-    }
-
-    return output;
-  }, []);
-
-  const getItemFromContents = useCallback((id: string, property: ContentsProperty): string => {
-    let output: string;
-    const filteredItem = contentsJson.filter(item => item.id === id);   
-
-    if (filteredItem.length === 0) {
-      output = `Item "${id}.${property}" nao encontrado em contents.json`;
-    } else {
-      output = filteredItem[0][property];
-    }
-
-    return output;
-  }, []);
-
   const navigationOpen = useCallback(() => {
     document.querySelector('body')?.classList.add('navigation-open');
     document.querySelector('body')?.classList.add('no-scroll');
@@ -67,8 +32,6 @@ export function AppContextProvider(props: IAppContextProviderProps) {
 
   return (
     <AppContext.Provider value={{
-      getHtmlFromContents,
-      getItemFromContents,
       navigationOpen,
       navigationClose,
     }}>
