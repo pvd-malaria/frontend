78dccaf28e7ce505f5e244c0fd27c1a4c9963c6a
Willianson <contato@willianson.com.br>
2022-05-20 10:59:43 -0300

    feat(router): scroll top ao mudar de página com caminhos base semelhante.

--- src/core/Router.tsx
+++ src/core/Router.tsx
@@ -5,21 +5,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Inicial from '../pages/Inicial';
 import Sobre from '../pages/Sobre';
 import UiGuide from '../pages/UiGuide';
+import VisualizacoesDetalhes from '../pages/Visualizacoes/Detalhes';
 import VisualizacoesLista from '../pages/Visualizacoes/Lista';
 
 // import layout temporary
 import Layout from '../components/Layout';
+import ScrollToTop from '../components/ScrollToTop';
 
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <BrowserRouter >
+      <ScrollToTop/>
+      <Routes >
         <Route path="/" element={<Inicial/>} />
         <Route path="/sobre" element={<Sobre/>} />
         <Route path="/dashboard" element={<Layout><h1>Dashboard</h1></Layout>} />
         <Route path="/modelos" element={<Layout><h1>Modelos</h1></Layout>} />
         <Route path="/producoes" element={<Layout><h1>Producoes</h1></Layout>} />
+        <Route path="/visualizacoes/:id" element={<VisualizacoesDetalhes/>} />
         <Route path="/visualizacoes" element={<VisualizacoesLista/>} />
         <Route path="/ui-guide" element={<UiGuide/>} />
         <Route path="*" element={<Layout><h1>Page not found</h1></Layout>} />
