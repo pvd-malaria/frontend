277f8ea4d3cc139e97a6fab234470c997cdfb2b0
Willianson <contato@willianson.com.br>
2022-06-07 08:57:57 -0300

    feat: pagina artigos.
    - Adiciona arquivo modelo html em /public.
    - Adiciona rota.
    - Adiciona pagina lista.
    - Adiciona pagina detalhes.
    
    #28

---
+++ src/pages/Producoes/Artigos/Detalhes/index.tsx
@@ -0,0 +1,118 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import CircularProgress from '@mui/material/CircularProgress';
+import DownloadIcon from '@mui/icons-material/Download';
+import DOMPurify from 'dompurify';
+import LinkIcon from '@mui/icons-material/Link';
+
+import Layout from '../../../../components/Layout';
+
+import jsonArtigos from '../../../../contents/artigos.json';
+import LinkButton from '../../../../components/LinkButton';
+
+import './styles.css';
+
+
+interface IArtigo {
+  id: string,
+  url: string,
+  urlPdf: string,
+  urlExternal: string,
+  date: string,
+  title: string,
+  short: string,
+}
+
+
+function ArtigosDetalhes() {
+
+  const [ artigo, setArtigo ] = useState<IArtigo>();
+  const [ htmlContent, setHtmlContent ] = useState('');
+  const [ loading, setLoading ] = useState(false);
+  const { id } = useParams<"id">();
+
+
+  const importContent = useCallback(async(file) => {
+    setLoading(true);
+    const headers = { 'Content-Type': 'Content-Type: text/html; charset=UTF-8' };
+    const response = await fetch(`${file}`, { headers });
+    const html = await response.text();
+    setHtmlContent(html);
+    setLoading(false);
+  }, [ setHtmlContent, setLoading ]);
+
+
+  useEffect(() => {
+    const [ filteredItem ] = jsonArtigos.list.filter(item => item.id === id);
+    console.log(filteredItem)
+    if (filteredItem) {
+      importContent(filteredItem.file);
+      setArtigo(filteredItem);
+    }
+  }, [id, importContent, setArtigo]);
+
+
+  // loading 
+  if (loading) {
+    return (
+      <Layout id="pageArtigosDetalhes">
+        <div className="container loading">
+          <CircularProgress />
+        </div>
+      </Layout>
+    );
+  }
+
+  // Detalhes
+  if (artigo) {
+    return (
+      <Layout id="pageArtigosDetalhes">
+        <section className="wrapperHeader">
+          <div className="container">
+            <h1>{artigo.title}</h1>
+            <p>{artigo.date}</p>
+          </div>
+        </section>      
+
+        <section className="wrapperArtigo">
+          <div className="container">
+            <div className="artigo">
+              <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(htmlContent)}}></div>
+            </div>
+
+            <LinkButton
+              style="none"
+              to="/producoes/artigos"
+              icon={<ArrowBackIosIcon/>}
+            >
+              Voltar para lista de artigos
+            </LinkButton>
+          </div>
+        </section>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout id="pageArtigosDetalhes">
+          <div className="container">
+            <h2>Ops... :(</h2>
+            <p>O artigo solicitado não foi encontrada.</p>
+            <br />
+
+            <LinkButton
+              style="none"
+              to="/producoes/artigos"
+              icon={<ArrowBackIosIcon/>}
+            >
+              Voltar para lista de artigos
+            </LinkButton>
+          </div>
+      </Layout>
+    );
+  }
+
+}
+
+export default ArtigosDetalhes;
---
+++ src/pages/Producoes/Artigos/Lista/index.tsx
@@ -0,0 +1,43 @@
+import Layout from '../../../../components/Layout';
+import Item from './Item';
+
+import jsonArtigos from '../../../../contents/artigos.json';
+
+import './styles.css';
+
+
+function ArtigosLista() {
+  return (
+    <Layout id="pageArtigosLista">
+      <section className="wrapperInfo">
+        <div className="container">
+          <h1>{jsonArtigos.page.title}</h1>
+          <p>{jsonArtigos.page.description}</p>
+        </div>
+      </section>
+
+      <section className="wrapperList">
+        <div className="container">
+          {
+            jsonArtigos.list
+              .map((item) =>
+                <Item
+                  date={item.date}
+                  key={item.id}
+                  linkTo={item.url}
+                  short={item.short}
+                  title={item.title}
+                />
+              )
+          }
+
+          {/* Hack para space-between */}
+          <article className="listItem"></article>
+        </div>
+      </section>
+
+    </Layout>
+  );
+}
+
+export default ArtigosLista;
