92403805d775aa0235edc624c489f4c258e58071
Willianson <contato@willianson.com.br>
2022-08-10 20:13:35 -0300

    refact(inicial): ajusta imagens de visualizacoes.

--- src/pages/Inicial/index.tsx
+++ src/pages/Inicial/index.tsx
@@ -11,8 +11,8 @@ import './styles.css';
 import imageTaxas from './images/taxas.png';
 import imageImportados from './images/importados.png';
 import imageVisualizationFirst from '../Visualizacoes/Lista/Item/images/bubbles.svg';
-import imageVisualizationSecond from '../Visualizacoes/Lista/Item/images/bars-filled-full.svg';
-import imageVisualizationThird from '../Visualizacoes/Lista/Item/images/map-network.svg';
+import imageVisualizationSecond from '../Visualizacoes/Lista/Item/images/lines-filled-full.svg';
+import imageVisualizationThird from '../Visualizacoes/Lista/Item/images/bars-filled-full.svg';
 
 
 function Inicial() {
