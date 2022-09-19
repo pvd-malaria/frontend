fe4dddb0f4999d046b28734e8c866ffe85f89ebe
Carlos Eduardo Beluzo <cbeluzo@gmail.com>
2022-09-19 11:38:51 -0300

    Minor changes.

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -55,10 +55,10 @@ function Sobre() {
               <p className="nome">Dra. Bianca Cechetto Carlos</p>
               <p className="email">bc.carlos@unesp.br</p>
               <p className="bio">Co-PI. Pesquisadora Visitante da UNESP. Bacharelado e Mestrado em Ciências Biológicas, Doutora em Ciências na área Biologia da Relação 
-                Patógeno-Hospedeiro, e Pós-doc atuando malária.</p>
+                Patógeno-Hospedeiro, e Pós-doc atuando com malária.</p>
             </div>
             <div className="membro">
-              <p className="nome">Jayme Augusto de Souza-Neto</p>
+              <p className="nome">Prof. Dr. Jayme Augusto de Souza-Neto</p>
               <p className="email">jaysneto@gmail.com</p>
               <p className="bio">Co-PI. Professor Assistente de Genômica Funcional da UNESP. Geneticista e Virologista atuando com Saúde e Bióloga Molecular. Atualmente é Cientista Sênior na CEEZAD/K-State University.</p>
             </div>
@@ -70,18 +70,18 @@ function Sobre() {
 
           <div className="listMembros">
             <div className="membro">
-              <p className="nome">Álvaro de Oliveira D'Antona</p>
+              <p className="nome">Prof. Dr. Álvaro de Oliveira D'Antona</p>
               <p className="email">alvaro.dantona@fca.unicamp.br</p>
               <p className="bio">Professor e Diretor Associado da Faculdade de Ciências Aplicadas da Unicamp. 
-                Formação em Ciências Humanas, Ciências Sociais Aplicadas, Economia, Antropologia, e, População e Ambiente. Pesquisador-colaborador do Núcleo de Estudos de População (NEPO-Unicamp).</p>
+                Formação em Ciências Humanas, Ciências Sociais Aplicadas, Economia, Antropologia, e, População e Ambiente.</p>
             </div>
             <div className="membro">
               <p className="nome">Everton Josué da Silva</p>
               <p className="email">evertonjsilva31@gmail.com</p>
-              <p className="bio">Professor no Instituto Federal de São Paulo na área de Informática e Ciência de Dados.</p>
+              <p className="bio">Pesquisadora Associada e Cientista de Dados. Professor no Instituto Federal de São Paulo na área de Informática e Ciência de Dados.</p>
             </div>
             <div className="membro">
-              <p className="nome">Tiago José de Carvalho</p>
+              <p className="nome">Prof. Dr. Tiago José de Carvalho</p>
               <p className="email">tiagojc@gmail.com</p>
               <p className="bio">Professor no Instituto Federal de São Paulo na área de Informática e Ciência de Dados.</p>
             </div>
@@ -93,10 +93,7 @@ function Sobre() {
             <div className="membro">
               <p className="nome">Vinícius de Souza Maia</p>
               <p className="email">viniciusmaia108@gmail.com</p>
-              <p className="bio">Pesquisador Associado. Bacharel em Ciências Sociais e Mestre em Demografia pela UNICAMP.
-
-B
-                ambos pela Universidade Estadual de Campinas.</p>
+              <p className="bio">Pesquisador Associado. Bacharel em Ciências Sociais e Mestre em Demografia pela UNICAMP.</p>
             </div>
           </div>
         </div>
@@ -113,7 +110,7 @@ B
             <div className="membro">
               <p className="nome">Willianson Araújo</p>
               <p className="email">contato@willianson.com.br</p>
-              <p className="bio">Cientista da Computação</p>
+              <p className="bio">Analista de Desenvolvimento Web</p>
             </div>
           </div>
         </div>
