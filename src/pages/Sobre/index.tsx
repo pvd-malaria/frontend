4dc47dc8a85117da299e14e2e4c68a8cb1379555
Carlos Eduardo Beluzo <cbeluzo@gmail.com>
2022-09-19 11:28:29 -0300

    Atualização de informações de contato

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -42,24 +42,25 @@ function Sobre() {
 
           <div className="listMembros">
             <div className="membro">
-              <p className="nome">Luciana Correia Alves</p>
+              <p className="nome">Profa. Dra. Luciana Correia Alves</p>
               <p className="email">lcalves@unicamp.br</p>
-              <p className="bio">Unicamp</p>
+              <p className="bio">PI. Chefe do Departamento de Demografia/IFCH - Universidade Estadual de Campinas.</p>
             </div>
             <div className="membro">
-              <p className="nome">Carlos Eduardo Beluzo</p>
-              <p className="email">cbeluzo@synapse.org</p>
-              <p className="bio">IFSP-Campinas | Unicamp</p>
+              <p className="nome">Prof. Me. Carlos Eduardo Beluzo</p>
+              <p className="email">beluzo@ifsp.edu.br</p>
+              <p className="bio">Co-PI. Professor no Instituto Federal de São Paulo na área de Informática e Ciência de Dados.</p>
             </div>
             <div className="membro">
-              <p className="nome">Bianca Cechetto Carlos</p>
-              <p className="email">---</p>
-              <p className="bio">UNESP</p>
+              <p className="nome">Dra. Bianca Cechetto Carlos</p>
+              <p className="email">bc.carlos@unesp.br</p>
+              <p className="bio">Co-PI. Pesquisadora Visitante da UNESP. Bacharelado e Mestrado em Ciências Biológicas, Doutora em Ciências na área Biologia da Relação 
+                Patógeno-Hospedeiro, e Pós-doc atuando malária.</p>
             </div>
             <div className="membro">
               <p className="nome">Jayme Augusto de Souza-Neto</p>
-              <p className="email">---</p>
-              <p className="bio">UNESP</p>
+              <p className="email">jaysneto@gmail.com</p>
+              <p className="bio">Co-PI. Professor Assistente de Genômica Funcional da UNESP. Geneticista e Virologista atuando com Saúde e Bióloga Molecular. Atualmente é Cientista Sênior na CEEZAD/K-State University.</p>
             </div>
           </div>
         </div>
@@ -70,28 +71,32 @@ function Sobre() {
           <div className="listMembros">
             <div className="membro">
               <p className="nome">Álvaro de Oliveira D'Antona</p>
-              <p className="email">---</p>
-              <p className="bio">Unicamp</p>
+              <p className="email">alvaro.dantona@fca.unicamp.br</p>
+              <p className="bio">Professor e Diretor Associado da Faculdade de Ciências Aplicadas da Unicamp. 
+                Formação em Ciências Humanas, Ciências Sociais Aplicadas, Economia, Antropologia, e, População e Ambiente. Pesquisador-colaborador do Núcleo de Estudos de População (NEPO-Unicamp).</p>
             </div>
             <div className="membro">
               <p className="nome">Everton Josué da Silva</p>
-              <p className="email">---</p>
-              <p className="bio">IFSP-Campinas | Unicamp</p>
+              <p className="email">evertonjsilva31@gmail.com</p>
+              <p className="bio">Professor no Instituto Federal de São Paulo na área de Informática e Ciência de Dados.</p>
             </div>
             <div className="membro">
               <p className="nome">Tiago José de Carvalho</p>
-              <p className="email">---</p>
-              <p className="bio">---</p>
+              <p className="email">tiagojc@gmail.com</p>
+              <p className="bio">Professor no Instituto Federal de São Paulo na área de Informática e Ciência de Dados.</p>
             </div>
             <div className="membro">
               <p className="nome">Natália Martins Arruda</p>
-              <p className="email">---</p>
-              <p className="bio">FIOCRUZ | Unicamp</p>
+              <p className="email">natimarruda@gmail.com</p>
+              <p className="bio">Pesquisadora Associada e Cientista de Dados. Bacharel em Economia e Mestre em Demografia pela UNICAMP.</p>
             </div>
             <div className="membro">
               <p className="nome">Vinícius de Souza Maia</p>
-              <p className="email">---</p>
-              <p className="bio">---</p>
+              <p className="email">viniciusmaia108@gmail.com</p>
+              <p className="bio">Pesquisador Associado. Bacharel em Ciências Sociais e Mestre em Demografia pela UNICAMP.
+
+B
+                ambos pela Universidade Estadual de Campinas.</p>
             </div>
           </div>
         </div>
@@ -102,13 +107,13 @@ function Sobre() {
           <div className="listMembros">
             <div className="membro">
               <p className="nome">Adriano Souza</p>
-              <p className="email">---</p>
-              {/* <p className="bio">Analista de Desenvolvimento Web</p> */}
+              <p className="email">silva.souza.adriano@gmail.com</p>
+              <p className="bio">Analista de Desenvolvimento Web</p>
             </div>
             <div className="membro">
               <p className="nome">Willianson Araújo</p>
               <p className="email">contato@willianson.com.br</p>
-              {/* <p className="bio">Cientista da Computação</p> */}
+              <p className="bio">Cientista da Computação</p>
             </div>
           </div>
         </div>
