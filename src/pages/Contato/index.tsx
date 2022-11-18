0d7b4f4a769b62df22a03f8e59304113a084c84c
Willianson <contato@willianson.com.br>
2022-11-18 09:14:09 -0300

    feat: página contato, endereço e mapa.
    closes #42

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -12,24 +12,33 @@ function Contato() {
 
       <section className="conteudo">
         <div className="container">
-          <h1>Fale conosco</h1>
-
-          <h2>LADDEM</h2>
-          <h4>Laboratório de Análise de Dados em Demografia</h4>
+          <h1>LADDEM</h1>
+          <h2>Laboratório de Análise de Dados em Demografia</h2>
+          <p>
+            <strong>E-mail:</strong> laddem.unicamp@gmail.com
+          </p>
           
-          <h2>UNICAMP</h2>
-          <h4>Universidade Estadual de Campinas</h4>
+          <br />
+
+          <h2>UNICAMP - Universidade Estadual de Campinas</h2>
           <div className="info">
             <address>
-              Núcleo de Estudos de População "Elza Berquó"<br/>
+              <strong>Núcleo de Estudos de População "Elza Berquó"</strong><br/>
               Av. Albert Einstein, 1300, sala 25<br/>
               Cidade Universitária Zeferino Vaz<br/>
-              CEP 13083-852 - Campinas/SP
+              CEP: 13083-852 <br/>
+              Campinas, SP
             </address>
-            <p>
-              E-mail: laddem.unicamp@gmail.com
-            </p>
           </div>
+
+          <iframe
+            id="frameMapa"
+            width="100%"
+            height="500"
+            frameBorder="0"
+            scrolling="no"
+            src="https://maps.google.com/maps?q=N%C3%BAcleo%20de%20Estudos%20de%20Popula%C3%A7%C3%A3o%20%22Elza%20Berqu%C3%B3%22%20Av.%20Albert%20Einstein,%201300,%20sala%2025%20Cidade%20Universit%C3%A1ria%20Zeferino%20Vaz%20CEP%2013083-852%20-%20Campinas/SP&t=&z=15&ie=UTF8&iwloc=&output=embed"
+          ></iframe>
         </div>
       </section>
 
