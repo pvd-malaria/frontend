95d61588cd562169f2b6d7b5c13645e05dd0053b
vct-h-dias <d.hugo@aluno.ifsp.edu.br>
2023-05-17 10:09:20 -0300

    CasesPerMonth tab changes and IncidateRate log add

---
+++ src/plotly-visualizations/Visualizacoes/Incidência/bubble_rate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChartIncidenceRate } from '../../components/charts';
+
+const VisCasesPerMonth = () => {
+
+	return (
+		<div style={{width: '100%', height: '100%'}}>
+			<ChartIncidenceRate/>
+		</div>
+	);
+};
+
+const div = document.getElementById('root');
+const root = createRoot(div);
+root.render(<VisCasesPerMonth/>);
--- src/plotly-visualizations/Visualizacoes/Incidência/incidence.js
+++ src/plotly-visualizations/Visualizacoes/Incidência/incidence.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChartIncidenceRate } from '../../components/charts';
+import { Tabs, Tab, Box } from '@mui/material';
+
+function a11yProps(index) {
+	return {
+		id: `simple-tab-${index}`,
+		'aria-controls': `simple-tabpanel-${index}`,
+	};
+}
 
 const VisIncidence = () => {
+	const [tab, setTab] = React.useState(0);
 
 	return (
 		<div style={{width: '100%', height: '100%'}}>
-			<ChartIncidenceRate/>
+			<Box sx={{
+				width: '100%',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center'
+			}}>
+				<Tabs value={tab} onChange={(e, nv) => setTab(nv)} aria-label="incidencia tabs">
+					<Tab label="Incidence Rate" {...a11yProps(0)} />
+					<Tab label="Incidence Rate (Logs)" {...a11yProps(1)} />
+				</Tabs>
+			</Box>
+			<ChartIncidenceRate hasLogs={tab === 1}/>
 		</div>
 	);
 };
