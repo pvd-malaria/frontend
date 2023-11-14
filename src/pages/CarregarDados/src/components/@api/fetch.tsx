7968ba11f4f6f2f5c3381310775b51ab183a7c12
lucasrdrgs <lucasrodriguespimentel@gmail.com>
2023-11-14 10:45:38 -0300

    Fix formatting, add /api-poc prefix

--- src/pages/CarregarDados/src/components/@api/fetch.tsx
+++ src/pages/CarregarDados/src/components/@api/fetch.tsx
@@ -1,45 +1,46 @@
 //export fuction to make fetch whatever route using http://72.44.53.85:9000
 
-
-
-export const requestApi = async (route: string, method: string, body?: any, multipart?: boolean) => {
-
-    const token =await localStorage.getItem('token');
-    const response = await fetch(  route, 
-    {
-        method: method,
-        headers: multipart ?{ "Authorization": token ? `Bearer ${token}` : "",  }:
-         {
-            mode: 'cors', // no-cors, cors, *same-origin
-            "Content-Type":  
-             "application/json", 
-            "Authorization": token ? `Bearer ${token}` : "",  
+export const requestApi = async (
+  route: string,
+  method: string,
+  body?: any,
+  multipart?: boolean
+) => {
+  const token = await localStorage.getItem("token");
+  const response = await fetch(route, {
+    method: method,
+    headers: multipart
+      ? { Authorization: token ? `Bearer ${token}` : "" }
+      : {
+          mode: "cors", // no-cors, cors, *same-origin
+          "Content-Type": "application/json",
+          Authorization: token ? `Bearer ${token}` : "",
         },
-        body:  multipart ? body :
-        body ? JSON.stringify(body) : null
-    }); 
-    const jsonParse = await response.body?.getReader().read().then(({ value }) => new TextDecoder("utf-8").decode(value));
-     if (response.status === 401 ) {
-        localStorage.removeItem('token');
-        window.location.reload();
+    body: multipart ? body : body ? JSON.stringify(body) : null,
+  });
+  const jsonParse = await response.body
+    ?.getReader()
+    .read()
+    .then(({ value }) => new TextDecoder("utf-8").decode(value));
+  if (response.status === 401) {
+    localStorage.removeItem("token");
+    window.location.reload();
 
-        const error = {
-            data: null,
-            statusCode: response.status,
-            message: response.statusText
-        }
-        return error;
-    }
-     else if (response.status !== 200 ) {
-        const error = {
-            data: null,
-            statusCode: response.status,
-            message: response.statusText
-        }
-        return error;
-    } 
-       
-    const data = {data: jsonParse,  statusCode: response.status};
-    return data;
-}
+    const error = {
+      data: null,
+      statusCode: response.status,
+      message: response.statusText,
+    };
+    return error;
+  } else if (response.status !== 200) {
+    const error = {
+      data: null,
+      statusCode: response.status,
+      message: response.statusText,
+    };
+    return error;
+  }
 
+  const data = { data: jsonParse, statusCode: response.status };
+  return data;
+};
